0ed25b62467d6d522fc97da1c878f16b3ee517ca
Rajani <rkpradhan462@gmail.com>
2020-08-27 10:45:26 +0200

    download

---
+++ code/dev/01_download.R
@@ -0,0 +1,13 @@
+setwd("C:/Users/rkpra/OneDrive/Documents/R_projects/GPM_download")
+
+fil <- "C:/Users/rkpra/OneDrive/Documents/R_projects/GPM_download/subset_GPM_3IMERGDF_06_20200826_193405.txt"
+url <- scan(fil, what = list(""), flush = TRUE)
+urls <- unlist(url)
+#t = basename(urls)
+names = substr(urls, 228, 280) #subset the character for naming the files
+library(httr)
+for (i in 1:length(urls)){
+  GET(as.character(urls[i]),
+      authenticate("rkpradhan462@gmail.com", "Rajani8099"),
+      write_disk(path = names[i], overwrite = T))
+}
