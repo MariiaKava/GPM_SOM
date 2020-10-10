0fdc2f6ce8d5e5096edb1bcfb50eb994e083e3cd
MariiaKava <marykavalerova@gmail.com>
2020-10-10 13:09:49 +0200

    Added a function to merge nc4s in one data table file Date and time variable is corrected Only precipitations >0 are gathered

---
+++ code/dev/functions.R
@@ -0,0 +1,36 @@
+source("code/source.R")
+#----Merge nc4 files----
+#Excract and merge date,lon,lag and precipitation into data table
+merge_nc4 <- function(i){
+  
+  file <-ncdf4::nc_open(paste0(instaldata_path,all_nc4[i]))
+  precipitation_data <- ncdf4::ncvar_get(file)
+  
+  date <- as.POSIXlt("1970-01-01 00:00")+ file$dim$time$vals
+  
+  dimnames(precipitation_data)[[1]] <- file$dim$lat$vals
+  dimnames(precipitation_data)[[2]] <- file$dim$lon$vals
+  
+  kk <- ncdf4::nc_close(file)
+  
+  
+  precip_summary <- data.table::data.table(reshape2::melt(precipitation_data,
+                                                          varnames = c("lat", "lon"),
+                                                          value.name = "precipitation"))
+  precip_summary <- precip_summary[complete.cases(precip_summary), ] 
+  
+  precip_summary$precipitation[precip_summary$precipitation<1]<-0 #Set all values with precipitation < 1mm to 0
+  
+  if (any(precip_summary$precipitation >0)){
+  
+  precip_summary <- precip_summary[precip_summary$precipitation>0,]
+  
+  precip_summary <- precip_summary[complete.cases(precip_summary), ]
+  
+  precip_summary <- cbind(precip_summary,date)
+  
+  return (precip_summary)
+  }
+  else{ return() }
+  
+}
